9d1e79748f9420238ceee36f004850fef7b060ba
Midori Bowen <midori.p.bowen@gmail.com>
2015-10-15 02:06:51 -0700

    debugging attempt 1.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
     var pets = $("input#pets").val();
     var kids = $("input#kids").val();
 
-    if (age >= 75 && pets === "None" && kids === "No") {
+    if (age >= 75) {
       $("#elderly").show();
-    } else if (age <= 74 && pets === "Cat" && pets === "Dog" && kids === "No") {
+    } else if (pets === "Cat") {
       $("#petPal").show();
-    } else if (age <= 74 && pets === "No" && kids === "Yes") {
+    } else if (pets === "Dog") {
+      $("#petPal").show();
+    }  else if (pets === "None" && kids === "Yes") {
       $("#kidFriendly").show();
-    } else if (age <= 74 && kids === "No" && pets === "None") {
+    } else {
       $("#solo").show();
     }
 
