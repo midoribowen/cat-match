f26d875caeff6e6e05af27e7995ed9fa89f2e713
Midori Bowen <midori.p.bowen@gmail.com>
2015-10-28 17:10:02 -0700

    Fix inputs so .shows show.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
   $("form#match").submit(function(event) {
     var age = parseInt($("input#age").val());
-    var pets = $("input#pets").val();
+    var pets = $("select#pets").val();
     var kids = $("input#kids").val();
 
-    if (age >= 75) {
-      $("#elderly").show();
-    } else if (pets === "Cat") {
-      $("#petPal").show();
-    } else if (pets === "Dog") {
-      $("#petPal").show();
-    }  else if (pets === "None" && kids === "Yes") {
-      $("#kidFriendly").show();
+    if (age > 75 || age === 75) {
+      $('#elderly').show();
+    } else if (pets === 'cat' && age < 75) {
+      $('#petPal').show();
+    } else if (pets === 'dog' && age < 75) {
+      $('#petPal').show();
+    }  else if (pets === 'none' && kids === 'yes' && age < 75) {
+      $('#kidFriendly').show();
     } else {
-      $("#solo").show();
+      $('#solo').show();
     }
 
     event.preventDefault();
